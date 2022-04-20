98dee9c710f9563ea0bbe614f6a8a593b1ad715e
kishor-ak <kishorefgh06@gmail.com>
2022-04-20 17:39:06 +0530

    variable added

--- add1.java
+++ add1.java
@@ -2,6 +2,7 @@ public class add1 {
     public static void main(String[] args) {
         int a = 55;
         int b = 15;
-        System.out.println(a+b);
+        int c = 77;
+        System.out.println(a+b+c);
     }
 }
