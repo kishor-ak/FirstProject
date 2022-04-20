041ceb9158903924b9135cefdc402a0b7b12498e
kishor-ak <103488945+kishor-ak@users.noreply.github.com>
2022-04-20 18:05:34 +0530

    c value changed

--- add1.java
+++ add1.java
@@ -2,7 +2,7 @@ public class add1 {
     public static void main(String[] args) {
         int a = 55;
         int b = 15;
-        int c = 77;
+        int c = 88;
         int d = 15;
         System.out.println(a+b+c+d);
     }
